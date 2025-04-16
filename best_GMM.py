08957d43025a77fde190694fb7864807e0237e91
creastro <142521127+creastro@users.noreply.github.com>
2025-04-16 18:10:14 +0200

    Add files via upload

--- best_GMM.py
+++ best_GMM.py
@@ -1,5 +1,5 @@
 from GMMClassifier import GMMCLassifier
 
 
-def GMM(random_state):
+def GMM(random_state=42):
     return GMMClassifier(n_components=14, covariance_type='full', prob_scale = 3, random_state=random_state)
