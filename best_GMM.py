5c3585c00df0286935a57b01011985303a2bc86f
creastro <142521127+creastro@users.noreply.github.com>
2025-04-17 16:19:49 +0200

    Delete best_GMM.py

--- best_GMM.py
+++
@@ -1,5 +0,0 @@
-from GMMClassifier import GMMCLassifier
-
-
-def best_GMM(random_state=42):
-    return GMMClassifier(n_components=14, covariance_type='full', prob_scale = 3, random_state=random_state)
