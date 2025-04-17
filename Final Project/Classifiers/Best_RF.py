a6aa8b3f901d0590e314337e66a49eea3f978b49
anacecilialr1 <142429056+anacecilialr1@users.noreply.github.com>
2025-04-17 23:04:10 +0200

    Update Best_RF.py

--- Final Project/Classifiers/Best_RF.py
+++ Final Project/Classifiers/Best_RF.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 
 def best_RF(X, y, random_state = 42, verbose = True):
     
@@ -22,17 +22,19 @@ def best_RF(X, y, random_state = 42, verbose = True):
     """
 
     #Hyperparameter grid to be tested
-    params = {'n_estimators': list(range(100, 1000)),
-    'max_depth': list(range(1, 13)),
-    'min_samples_split': [2, 3, 4]}
+    params = {'n_estimators': range(100, 600, 100),
+    'max_depth': range(5, 20),
+    'min_samples_split': [2, 3, 4],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['sqrt', 'log2', None]}
     
     #Randomized search on hyper parameters
-    search = RandomizedSearchCV(RandomForestClassifier(random_state=random_state), params, cv=5, n_iter = 50, n_jobs=-1)
+    search = RandomizedSearchCV(RandomForestClassifier(random_state=random_state), params, cv=5, n_iter = 70, n_jobs=-1)
     #Fit the search to the data
     search.fit(X, y)
 
     #Find the best classifier
     best_RFmodel = search.best_estimator_
-    best_RFmodel.set_params(random_state = random_state)
+  
 
     return best_RFmodel
